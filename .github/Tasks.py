c41dc9ec681a9dc9d4f65d1a6aaf55186e9f1e8d
EmaanAhmad27 <emaanahmad327@gmail.com>
2024-08-22 22:23:10 +0000

    Python Assignment 1

---
+++ .github/Tasks.py
@@ -0,0 +1,50 @@
+birthyear: int = 2001
+Currentyear: int = 2024
+age = birthyear - Currentyear
+print (age)
+
+Length_of_rectangle: int = 8
+Width_of_rectangle: int = 5
+area = Length_of_rectangle * Width_of_rectangle
+print (area)
+
+π: float = 3.14
+Radius: int = 7
+Area_of_circle = π * Radius ** 2 
+print (Area_of_circle)
+
+Side: int = 4
+Area_of_cube = 6 * Side ** 2
+print (Area_of_cube)
+
+Fahrenheit: float = 98.6
+Celsius = Fahrenheit - 32 * 5 / 9
+print (Celsius)
+
+Celsius2: int = 37
+Fahrenheit2 = Celsius2 * 9 / 5 + 32
+print (Fahrenheit2)
+
+totalseconds: int = 500
+minutes = totalseconds // 60
+remaining_seconds = totalseconds % 60
+print (minutes)
+print (remaining_seconds)
+
+obtained_marks: int = 769
+total_marks: int = 1100
+percentage_value = (obtained_marks / total_marks) * 100
+print (percentage_value)
+
+weight: int = 64
+height: float = 1.62
+BMI = weight / height ** 2
+print (BMI)
+
+π: float = 3.14
+radius1: int = 3
+height1: int = 5
+Volume = π * radius1 ** 2 * height1
+print (Volume)
+
+
